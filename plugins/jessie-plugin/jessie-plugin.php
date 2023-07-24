c3b57d542701c1be3b7067cc81bcc1d60f76a3e8
jcnatividad <jcnatividad@gmail.com>
2023-07-24 20:15:53 +0800

    Add Base Plugin files

---
+++ plugins/jessie-plugin/jessie-plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Jessie Plugin
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     jessie-plugin
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Jessie_Plugin
+ */
+
+// Your code starts here.
